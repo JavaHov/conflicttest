3adfdfb6bafddcfe301b71186fd309f1de1e8467
Ola Lindgård <ola.lindgard@iths.se>
2021-03-14 16:12:48 +0100

    En till commit till 'anotherbranch'

--- GitTest.java
+++ GitTest.java
@@ -6,5 +6,6 @@ public class GitTest
     public static void main(String[] args)
     {
         System.out.println("Nu ska vi se om det blir fel på GitHub...");
+		System.out.println("Vi ändrar lite här då. Den här skickar vi till 'anotherbranch'...");
     }
 }
